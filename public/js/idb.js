70ad77cc676ba9882383cc169b933fde6eaefa48
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-07-29 20:42:53 -0700

    added requests to idb file

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,2 +1,27 @@
+// variable to hold db
 let db;
 
+// creates connection to IndexdDB
+const request = indexedDB.open('budget', 1);
+
+// this will emit if the database version is changed
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+
+    // creates an object store (table) called 'transaction' that has an auto incrementing primary key
+    db.createObjectStore('transaction', { autoIncrement: true });
+};
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+
+    // checks if app is online and if it is uploadTransaction will send all local data to api
+    if (navigator.onLine) {
+        uploadTransaction();
+    }
+};
+
+request.onerror = function(event) {
+    // logs error
+    console.log(event.target.errorCode);
+};
